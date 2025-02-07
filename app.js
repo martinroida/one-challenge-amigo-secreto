4e4c02fb40f8d81b67f740142fe95eb293a31ea2
Martin ROIDA <martindarioroida@gmail.com>
2025-02-06 22:41:06 -0300

    :sparkles: feat: se agregaron las funciones agregarAmigo() y limpiaCaja() al archivo app.js

--- app.js
+++ app.js
@@ -3,3 +3,22 @@
 //Declaracion de vector para guardar amigos: en esta lista van a ir almacenados los amigos que se van a ir cargando.
 
 let amigos = [];
+
+//Funcion para agregar amigos al hacer click en el botón añadir:
+
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    //Validación: verifica si la caja está vacía.
+    //to-do: ver de que forma implementar una validación con números, simbolos, etc.
+    if (amigo == ""){
+        alert('Por favor, ingrese un nombre.');
+    } else {
+        amigos.push(amigo);
+        limpiaCaja();
+    }
+    }
+
+    //Funcion para limpiar caja de texto:
+    function limpiaCaja(){
+        document.querySelector('#amigo').value = '';
+    }
