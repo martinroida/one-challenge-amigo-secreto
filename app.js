943d34ff34de41ce8677e1cb4e59182e6e50e336
Martin ROIDA <martindarioroida@gmail.com>
2025-02-06 22:17:04 -0300

    :sparkles: feat: se agrego la lista amigos[] al archivo app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Declaracion de vector para guardar amigos: en esta lista van a ir almacenados los amigos que se van a ir cargando.
+
+let amigos = [];
