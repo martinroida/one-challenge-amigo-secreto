4573f668b9f5f1fc668812207099a64f00ddea62
Martin ROIDA <martindarioroida@gmail.com>
2025-02-13 07:04:38 -0300

    :sparkles: feat: se agrego la funcion mostrarLista() al archivo app.js

--- app.js
+++ app.js
@@ -4,9 +4,14 @@
 
 let amigos = [];
 
+//Declaración de variable: captura en una variable la lista <ul></ul> (a través de su id) donde se van a insertar los amigos.
+
+let listaAmigos = document.getElementById('listaAmigos');
+
 //Funcion para agregar amigos al hacer click en el botón añadir:
 
 function agregarAmigo(){
+    //to-do: llevar esta variable por fuera de la función:
     let amigo = document.getElementById('amigo').value;
     //Validación: verifica si la caja está vacía.
     //to-do: ver de que forma implementar una validación con números, simbolos, etc.
@@ -14,11 +19,21 @@ function agregarAmigo(){
         alert('Por favor, ingrese un nombre.');
     } else {
         amigos.push(amigo);
-        limpiaCaja();
+        limpiarCaja();
+        mostrarLista();
     }
-    }
+}
 
-    //Funcion para limpiar caja de texto:
-    function limpiaCaja(){
-        document.querySelector('#amigo').value = '';
+//Funcion para limpiar caja de texto:
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+}
+
+//Funcion para mostrar la lista de amigos en index.html:
+
+function mostrarLista(){
+    listaAmigos.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        listaAmigos.innerHTML += `<li>${amigos[i]}</li>`;
     }
+}
